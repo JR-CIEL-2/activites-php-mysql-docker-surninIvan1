d47087883e6ae4a4996544c354f2ec5ad732e551
surninIvan1 <144660292+surninIvan1@users.noreply.github.com>
2024-11-20 13:37:09 +0000

    Exercice tp

---
+++ main.php
@@ -0,0 +1,14 @@
+<?php
+
+include 'statistique.php';
+
+$moyenneNotes = moyenne($tab);
+
+echo 'La moyenne est de ' . $moyenneNotes;
+
+
+
+$moyenneNotes = médiane($tab2);
+
+echo 'La médiane est de ' . $Notes;
+?>
---
+++ statistique.php
@@ -0,0 +1,39 @@
+<?php
+
+
+function moyenne($notes) {
+    if (count($notes) === 0) {
+        return 0; 
+    }
+    return array_sum($notes) / count($notes);
+}
+
+
+$tab = [15, 18, 9];
+$moyenneValeur = moyenne($tab);
+echo 'La moyenne est de ' . $moyenneValeur ,' et ';
+
+function médiane($notes) {
+ 
+    if (count($notes) === 0) {
+        return 0;
+    }
+
+
+    sort($notes);
+    
+    $count = count($notes);
+    $middleIndex = floor($count / 2); 
+
+    if ($count % 2 === 1) {
+        return $notes[$middleIndex];
+    } else {
+        return ($notes[$middleIndex - 1] + $notes[$middleIndex]) / 2; 
+    }
+}
+
+$tab2 = [5, 7, 9, 12, 34, 45];
+$médianeValeur = médiane($tab2);
+echo 'La médiane est de ' . $médianeValeur;
+
+?>
