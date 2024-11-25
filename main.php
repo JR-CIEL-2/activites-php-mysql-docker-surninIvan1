8a9f63d8d79fe284d86986a81860fe58bed5dc8d
surninIvan1 <144660292+surninIvan1@users.noreply.github.com>
2024-11-25 15:57:34 +0000

    0.1 , 0.2

--- main.php
+++ main.php
@@ -2,13 +2,5 @@
 
 include 'statistique.php';
 
-$moyenneNotes = moyenne($tab);
 
-echo 'La moyenne est de ' . $moyenneNotes;
-
-
-
-$moyenneNotes = médiane($tab2);
-
-echo 'La médiane est de ' . $Notes;
 ?>
